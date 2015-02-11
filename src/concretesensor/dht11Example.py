0cf2c0848af2f9bc7a8a33395d866f61b29c2d69
Júnior <juniorug@gmail.com>
2015-02-11 01:40:44 -0300

    adicionado arquivo de teste

---
+++ src/concretesensor/dht11Example.py
@@ -0,0 +1,12 @@
+'''
+Created on 11/02/2015
+
+@author: zeus
+'''
+
+from concretesensor import DHT11
+
+if __name__ == '__main__':
+    dht11 = DHT11()
+    dht11.getTemperature()
+    dht11.getHumidity() 
---
+++ src/concretesensor/hcsr04Example.py
@@ -0,0 +1,10 @@
+'''
+Created on 11/02/2015
+
+@author: zeus
+'''
+from concretesensor import HCSR04
+
+if __name__ == '__main__':
+    HCSR04 = HCSR04()            
+    print (HCSR04.measure(0))
