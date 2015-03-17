26118817ad1756914f3f7e243d19b453ab2a2522
Júnior <juniorug@gmail.com>
2015-03-16 23:15:54 -0300

    adicionando classes de exemplo

--- examples/temperatureExample.py
+++ examples/temperatureExample.py
@@ -9,4 +9,4 @@ from concretefactory.temperatureSensorFactory import TemperatureSensorFactory
 if __name__ == '__main__':
 
     dht11 = TemperatureSensorFactory.createSensor("DHT11Temperature")
-    print (dht11.getTemperature())
+    print (dht11.getTemperatureInFahrenheit())
