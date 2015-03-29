2bc49907e0b604133fd35479de278a5c8e2e6bfa
Júnior <juniorug@gmail.com>
2015-03-29 20:51:51 -0300

    corrigindo concatenacao de string

--- examples/hysrf05Example.py
+++ examples/hysrf05Example.py
@@ -9,6 +9,6 @@ from concretefactory.ultrasonicSensorFactory import UltrasonicSensorFactory
 if __name__ == '__main__':
 
     hysrf05 = UltrasonicSensorFactory.createSensor("HYSRF05")
-    print ("Distance in cm:" + hysrf05.distance_in_cm() + " cm")
-    print ("Distance in inches:" + hysrf05.distance_in_inches() + " in")
+    print ("Distance in cm:" + str(hysrf05.distance_in_cm()) + " cm")
+    print ("Distance in inches:" + str(hysrf05.distance_in_inches()) + " in")
 
