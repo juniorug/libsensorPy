70a7999f21ae31b95a16a1a76a476045f5435559
juniorug <juniorug@gmail.com>
2015-04-22 16:54:21 -0300

    alterando html

---
+++ doc/bak/html/pages/principal.php
@@ -0,0 +1,8 @@
+<html>
+ <head>
+  <title>PHP Teste</title>
+ </head>
+ <body>
+ <?php echo "<p>Olá Mundo! voce está em principal.php</p>"; ?>
+ </body>
+</html>
---
+++ doc/html/indexBAK.php
@@ -0,0 +1,10 @@
+<?php require_once('url.php') ?>
+<html>
+ <head>
+  <title>libsensorPy</title>
+ </head>
+ <body>
+ <?php echo "<p>LibsensorPy</p>"; ?>
+ <?php include $folder.'/'.$page.'.php'; ?>
+ </body>
+</html>
